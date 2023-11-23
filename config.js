f9929536b52443707e46604c752f2c343f4734aa
Rashmika001rash <149502491+Rashmika001rash@users.noreply.github.com>
2023-11-23 14:37:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-import { watchFile, unwatchFile } from 'fs'
+hiimport { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
@@ -7,9 +7,9 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
-  [''] 
+  ['94778836398', 'Rashi Official²⁰²³', true],
+  ['94778836398', 'Rashi Official²⁰²³', true], 
+  ['94778836398'] 
 ] //Number of owners
 
 global.mods = ['94767939688','94767939688'] 
@@ -49,8 +49,8 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = '💝 Queen Hentai 💝'
 global.premium = 'true'
-global.packname = '💝 Queen Hentai 💝 Created By 💝 Dinuwa Official²⁰²³ 💝' 
-global.author = '💝 Dinuwa Official²⁰²³ 💝' 
+global.packname = '💝 Queen Hentai 💝 Created By 💝 Rashi Official²⁰²³ 💝' 
+global.author = '💝 Rashi Official²⁰²³ 💝' 
 global.igfg = '▢ Follow on Facebook\https://www.facebook.com/profile.php?id=61551915615888\n' 
 global.dygp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
 global.fgsc = 'https://github.com/dinuwah/QUEEN-HENTAI' 
